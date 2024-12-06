c7777557d7f679ca858fbeb2be78e74d79da916b
Your Name <you@example.com>
2024-12-06 02:11:07 -0900

    tast

--- controller/cartController.js
+++ controller/cartController.js
@@ -96,7 +96,7 @@ export const removeCartItem  = async(req, res) => {
 
         for (let index = 0; index < user.cart.length; index++) {
             if (user.cart[index].product._id.equals(product.Product._id)) {
-                if (user.cart[i].quantity == 1) {
+                if (user.cart[index].quantity == 1) {
                     user.cart.splice(index, 1)
                     
                 }else{
